76abb433660adaf035ca4ba06899102dcff37726
AntCannon <anthonycannonier@gmail.com>
2024-06-18 20:32:18 -0400

    add index route

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const colors = require('./models/color')
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send("Welcome to my Express app")
+})
+
+app.get('/colors', (req, res) => {
+  res.send(colors)
 })
 
 module.exports = app
