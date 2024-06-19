94236beba30123bf46500e91fe61b68c748149e2
AntCannon <anthonycannonier@gmail.com>
2024-06-18 20:05:38 -0400

    add server.js set up

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./app.js')
+
+// config
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+// listen
+app.listen(PORT, () =>  {
+  console.log(`Listening on port ${PORT}`)
+})
